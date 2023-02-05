cf1de93341652fa9da7821df09f9f8ca282174a7
Gabriel Alves <111418442+Biel-Alves@users.noreply.github.com>
2023-02-06 00:24:57 +0100

    update

--- src/be/intecbrussel/MainApp.java
+++ src/be/intecbrussel/MainApp.java
@@ -4,27 +4,74 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("Extra Oefeningen");
         System.out.println("---- Oefening 1 ----");
+        // 1. Schrijf een programma dat het brutoloon omrekent naar het nettoloon.
+        // Het programma moet rekening houden met de belastingen die ervan afgehouden worden.
+        //- Brutoloon groter dan 3000 euro = 38% belastingen.
+        //- Brutoloon tussen 2000 en 3000 euro= 35% belastingen.
+        //- Brutoloon kleiner dan 2000 euro= 27% belastingen.
 
-        int bruto =  50000;
+        int brutoloon =  5000;
 
         int belasting = 0;
         int result = 0;
 
-        if (bruto > 3000) {
-             belasting = bruto / 100 * 38;
-             result = bruto - belasting;
-            System.out.println("Neto = " + result);
-        } else if (bruto >= 2000 && bruto <= 3000 ) {
-            belasting = bruto / 100 * 35;
-            result = bruto - belasting;
-            System.out.println("Neto = " + result);
-        } else if (bruto < 2000) {
-            belasting = bruto / 100 * 27;
-            result = bruto - belasting;
-            System.out.println("Neto = " + result);
+        if (brutoloon > 3000) {
+             belasting = brutoloon / 100 * 38;
+             result = brutoloon - belasting;
+            System.out.println("Nettoloon = " + result + "€");
+        } else if (brutoloon >= 2000 && brutoloon <= 3000 ) {
+            belasting = brutoloon / 100 * 35;
+            result = brutoloon - belasting;
+            System.out.println("Nettoloon = " + result + "€");
+        } else if (brutoloon < 2000) {
+            belasting = brutoloon / 100 * 27;
+            result = brutoloon - belasting;
+            System.out.println("Nettoloon = " + result + "€");
         }
 
         System.out.println(" ---- oefening 2 ---- ");
+        // 2. Schrijf een programma dat van een gegeven bedrag in euro (1 tot 500 euro) berekent
+        // welke biljetten en stukken je nodig hebt om het bedrag uit te betalen en hoeveel van elk.
+
+        System.out.println(" ---- oefening 3 ---- ");
+        // 3. Schrijf een programma met 2 variabelen. Zorg dat je de basic calculaties (+, -, /, *, %) kunt uitvoeren.
+        // Gebruik hiervoor een switch statement.
+
+        int a = 65;
+        int b = 10;
+        int res = 0;
+
+        String operators = "*";
+
+        switch (operators) {
+            case "+" : {
+                res = a + b;
+                System.out.println(a + " + " + b + " = " + res );
+                break;
+            } case "-" : {
+                res = a - b;
+                System.out.println(a + " - " + b + " = " + res );
+                break;
+            } case "/" : {
+                res = a / b;
+                System.out.println(a + " / " + b + " = " + res );
+                break;
+            } case "*" : {
+                res = a * b;
+                System.out.println(a + " * " + b + " = " + res );
+                break;
+            } case "%" : {
+                res = a % b;
+                System.out.println(a + " % " + b + " = " + res );
+                break;
+            } default: {
+                System.out.println("Ongeldige operator!");
+            }
+        }
+
+        System.out.println(" ---- oefening 4 ---- ");
+        // 4. Maak een programma dat een geheel aantal seconden, bijvoorbeeld 5924, omrekent in uren, minuten en seconden.
+        // Het resultaat: U:1 M:38 S:44.
 
     }
 }
